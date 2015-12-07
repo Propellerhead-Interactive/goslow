f75d06fd0099c77dbd204db936d6ec1cc88daa82
mattkantor <matthewkantor@gmail.com>
2015-12-07 14:10:09 -0500

    working schedule

--- app/model/populate_routes.py
+++ app/model/populate_routes.py
@@ -1,11 +1,17 @@
 import urllib2
 import json
+from  models import TrainRoute, db
 from BeautifulSoup import BeautifulSoup
 
 
-
-
+def setupTables():
+    db.connect()
+    db.create_tables([TrainRoute], safe=True)
+    #db.create_tables([Status], safe=True)
+    db.commit()
+    
 def get_routes():
+    setupTables()
     routes = [151, 153, 155, 157, 159, 161, 203, 205, 207, 209, 279, 281, 285, 412, 414, 432, 434, 436, 442, 465, 479, 481, 483, 491, 493, 495, 718, 719, 720, 721, 722, 723, 725, 801, 803, 805, 807, 809, 829, 831, 833, 835, 858, 860, 862, 864, 866, 917, 918, 919, 920, 921, 922, 923, 924]
     src='http://gmcprod.metrolinx.com/pins//details/'
     for x in routes:
@@ -19,9 +25,11 @@ def get_routes():
             route_id = route["trip"]["id"]
             route_name = route["trip"]["name"]
             route_time = route["trip"]["time"]
-        
+            
             print route_id, route_name, route_time
-        
+            route = TrainRoute(route_name=route_name, route_id = route_id, route_time = route_time)
+            route.save()
+            
 get_routes()
         
     
