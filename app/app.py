4e1b738df9434ea3ed6cff4f780cd94608969568
mattkantor <matthewkantor@gmail.com>
2015-12-07 15:34:09 -0500

    schedule scrapes

--- app/app.py
+++ app/app.py
@@ -1,5 +1,9 @@
+import sys
 from flask import Flask
 from flask import render_template
+
+sys.path.insert(0, 'app/model/')
+from models import TrainRoute, Status
 
 app = Flask(__name__)
 app.config.update(
@@ -11,6 +15,11 @@ app.config.update(
 def hello():
     
     return render_template("index.html")
+
+@app.route("/schedule")
+def delays():
+    schedule = Status.get()
+    return render_template("delays.html", items=schedule)
 
 if __name__ == "__main__":
     app.run()
