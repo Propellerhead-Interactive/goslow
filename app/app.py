e92409e35057b51882cc6a9973a3149f127fce52
Phil Moreira <philm@beetsworking.ca>
2015-12-07 15:47:01 -0500

    playing around with rendering views

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+import sys
+sys.path.append( '../app/model/')
+from models import Tweet, db
 
 app = Flask(__name__)
 app.config.update(
@@ -9,8 +12,15 @@ app.config.update(
 
 @app.route("/")
 def hello():
+    words = {}
+    words['delay'] = Tweet.select().where(Tweet.content.contains('delay')).count()
+    words['sorry'] = Tweet.select().where(Tweet.content.contains('sorry')).count()
+    words['signal'] = Tweet.select().where(Tweet.content.contains('signal')).count()
+    words['switch'] = Tweet.select().where(Tweet.content.contains('switch')).count()
+    words['problem'] = Tweet.select().where(Tweet.content.contains('problem')).count()
+    words['issue'] = Tweet.select().where(Tweet.content.contains('issue')).count()
     
-    return render_template("index.html")
+    return render_template("index.html", words=words)
 
 if __name__ == "__main__":
     app.run()
