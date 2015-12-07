6a827d2536e4692e1ae1bdc433c81f5b6a9a0da3
Phil Moreira <philm@beetsworking.ca>
2015-12-07 15:11:46 -0500

    get last 3200 tweets on new table creation

--- script/get_gotransit_tweets.py
+++ script/get_gotransit_tweets.py
@@ -20,28 +20,38 @@ class Tweet(BaseModel):
     tweet_time = TextField()
     at_reply = BooleanField(default=True)
 
+def storeStatuses(st):
+  if st:
+    print "Added " + str(len(st)) + " new records."
+    with db.atomic():
+        for s in st:
+            atreply = True
+            if s.text[0] != "@":
+                atreply = False
+      
+            Tweet.create(content=s.text, at_reply=atreply, tweet_time=s.created_at_in_seconds, tweet_id=s.id)
+  else:
+    print "No new tweets"
+
 def main():
     db.connect()
     db.create_tables([Tweet], safe=True) 
 
     if Tweet.select().count() > 0:
       last_tweet = int(Tweet.select().order_by(-Tweet.tweet_time).limit(1).get().tweet_id)
-      statuses = api.GetUserTimeline(screen_name="gotransit",count=100, since_id=last_tweet)
+      statuses = api.GetUserTimeline(screen_name="gotransit",count=200, since_id=last_tweet)
     else:
-      print "Loading DB with last 200 tweets"
-      statuses = api.GetUserTimeline(screen_name="gotransit",count=200)
+      oldest_tweet = 0
+      statuses = []
+      count = 1
+      while count < 17:
+        print "Loading DB with 200 tweets..."
+        more_statuses = api.GetUserTimeline(screen_name="gotransit",count=200*count, max_id=oldest_tweet)
+        oldest_tweet = more_statuses[-1].id
+        statuses += more_statuses
+        count += 1
 
-    if statuses:
-        with db.atomic():
-            for s in statuses:
-                atreply = True
-                if s.text[0] != "@":
-                    atreply = False
-          
-                Tweet.create(content=s.text, at_reply=atreply, tweet_time=s.created_at_in_seconds, tweet_id=s.id)
-    else:
-      print "No new tweets"
-
+    storeStatuses(statuses)
     db.commit()
   
 main()
