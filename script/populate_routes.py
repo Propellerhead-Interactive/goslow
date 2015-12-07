32727c35cdd988fc4d6a42d5bb0cad74f404e2d7
mattkantor <matthewkantor@gmail.com>
2015-12-07 17:08:39 -0500

    unique train routes

--- script/populate_routes.py
+++ script/populate_routes.py
@@ -1,5 +1,6 @@
-import urllib2
+import urllib2, sys, json
 import json
+sys.path.insert(0, 'app/model/')
 from  models import TrainRoute, db, Status
 from BeautifulSoup import BeautifulSoup
 
@@ -12,8 +13,8 @@ def setupTables():
     
 def get_routes():
     setupTables()
-    routes = [151, 153, 155, 157, 159, 161, 203, 205, 207, 209, 279, 281, 285, 412, 414, 432, 434, 436, 442, 465, 479, 481, 483, 491, 493, 495, 718, 719, 720, 721, 722, 723, 725, 801, 803, 805, 807, 809, 829, 831, 833, 835, 858, 860, 862, 864, 866, 917, 918, 919, 920, 921, 922, 923, 924]
-    src='http://gmcprod.metrolinx.com/pins//details/'
+    routes = range(150,950)
+    src='http://gmcprod.metrolinx.com/pins/details/'
     for x in routes:
         newsrc=src + str(x)
         page = urllib2.urlopen(newsrc)
@@ -27,9 +28,8 @@ def get_routes():
             route_time = route["trip"]["time"]
             
             print route_id, route_name, route_time
-            route = TrainRoute(route_name=route_name, route_id = route_id, route_time = route_time)
-            route.save()
-            
+            route = TrainRoute.get_or_create(route_name=route_name, route_id = route_id, route_time = route_time)
+           
 get_routes()
         
     
