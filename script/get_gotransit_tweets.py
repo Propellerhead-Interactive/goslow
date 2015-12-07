c6f5851ea3ac9d9f6c37ccfad460fdeb926ed156
Phil Moreira <philm@beetsworking.ca>
2015-12-07 15:23:58 -0500

    refactoring

--- script/get_gotransit_tweets.py
+++ script/get_gotransit_tweets.py
@@ -1,24 +1,11 @@
 #Imports
+import sys
+sys.path.append( '../app/model/')
+from models import Tweet, db
 import twitter
-import peewee
-from peewee import *
-import datetime
 
 # Twitter Logic
 api = twitter.Api(consumer_key='zKWUxAituSEtELXZTUFZIpvN6',consumer_secret='DLtc8CJMCkGMHEChhwuEjuXM7OH0WzaJhZ1XyTSTPr7gxvnelz',access_token_key='11184792-wo0VQ63FhjVOsOgs28Mqk77CH73PB2KShCP8DwBKF',access_token_secret='PSAaONv8W7S8HJKWUMxappcr3khcAV2nlQfJPjzm5iZbN')
-
-#DB Logic
-db = MySQLDatabase('goslow', user='root',passwd='')
-
-class BaseModel(peewee.Model):
-    class Meta:
-        database = db
-
-class Tweet(BaseModel):
-    content = TextField()
-    tweet_id = TextField()
-    tweet_time = TextField()
-    at_reply = BooleanField(default=True)
 
 def storeStatuses(st):
   if st:
