46dd0740dadf7556d853814889a9f47863405756
Phil Moreira <philm@beetsworking.ca>
2015-12-07 14:24:44 -0500

    requirements for mysqldb and updated script for tweets

--- script/get_gotransit_tweets.py
+++ script/get_gotransit_tweets.py
@@ -1,6 +1,47 @@
+#Imports
 import twitter
+import peewee
+from peewee import *
+import datetime
+
+# Twitter Logic
 api = twitter.Api(consumer_key='zKWUxAituSEtELXZTUFZIpvN6',consumer_secret='DLtc8CJMCkGMHEChhwuEjuXM7OH0WzaJhZ1XyTSTPr7gxvnelz',access_token_key='11184792-wo0VQ63FhjVOsOgs28Mqk77CH73PB2KShCP8DwBKF',access_token_secret='PSAaONv8W7S8HJKWUMxappcr3khcAV2nlQfJPjzm5iZbN')
 
-statuses = api.GetUserTimeline(screen_name="gotransit",count=200)
+#DB Logic
+db = MySQLDatabase('goslow', user='root',passwd='')
 
-print [s.text for s in statuses]
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
+
+class Tweet(BaseModel):
+    content = TextField()
+    tweet_id = TextField()
+    tweet_time = TextField()
+    at_reply = BooleanField(default=True)
+
+def main():
+    db.connect()
+    db.create_tables([Tweet], safe=True) 
+
+    if Tweet.select().count() > 0:
+      last_tweet = int(Tweet.select().order_by(-Tweet.tweet_time).limit(1).get().tweet_id)
+      statuses = api.GetUserTimeline(screen_name="gotransit",count=100, since_id=last_tweet)
+    else:
+      print "Loading DB with last 200 tweets"
+      statuses = api.GetUserTimeline(screen_name="gotransit",count=200)
+
+    if statuses:
+        with db.atomic():
+            for s in statuses:
+                atreply = True
+                if s.text[0] != "@":
+                    atreply = False
+          
+                Tweet.create(content=s.text, at_reply=atreply, tweet_time=s.created_at_in_seconds, tweet_id=s.id)
+    else:
+      print "No new tweets"
+
+    db.commit()
+  
+main()
