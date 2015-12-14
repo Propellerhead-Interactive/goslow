5b9baa7aa665b2b466825d8e0a0c6d0b7f40d7fb
mattkantor <matthewkantor@gmail.com>
2015-12-14 14:34:58 -0500

    docs and templates for api

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import sys, json
 from flask import Flask, jsonify
+from flask.ext.autodoc import Autodoc
 from flask import render_template
 from playhouse.shortcuts import *
 import sys
@@ -8,6 +9,7 @@ sys.path.append( 'app/helper/')
 from search import TrainSearch
 from models import Tweet, db
 from utils import Utils
+
 
 sys.path.insert(0, 'app/model/')
 from models import Routes, Status
@@ -21,6 +23,7 @@ app.config.update(
     DEBUG=True,
     use_reloader=True
 )
+auto = Autodoc(app)
 
 assets_env = Environment(app)
 assets_loader = PythonAssetsLoader(assets)
@@ -56,11 +59,41 @@ def refund():
 @app.route("/api")
 def api():
     '''Doesnt do anything'''
-    return jsonify({"message":"usage:TBD"})
+    return render_template("api.html")
 
-#shows all the routes for the given system
+@app.route("/api/docs")
+def api_docs():
+    '''Doesnt do anything'''
+    return auto.html(
+        template='docs.html',
+        title='My Documentation',
+        author='John Doe'
+    )
+
+    
+@app.route("/api/keys")
+def api_key_index():
+    '''Doesnt do anything'''
+    return render_template("keys.html")
+    
+@app.route("/api/status")
+def api_status_index():
+    '''Doesnt do anything'''
+    return render_template("status.html")
+    
+@app.route("/api/keys/create", methods=['POST'])
+def api_key_create():
+    '''Adds a new Key to the system'''
+    #add key
+    
+    return render_template("keys.html")#, keys = keys)
+
+#================= Actual API ==============================
+
 @app.route("/api/<systemID>/routes",methods = ['GET'])
+@auto.doc()
 def all_routes(systemID):
+    """shows all the routes for the given system."""
     r = Routes.select().where(Routes.route_type==2)
     all_r = []
     for rr in r:
@@ -68,6 +101,7 @@ def all_routes(systemID):
     return jsonify({"routes":(all_r) })
 
 @app.route("/api/<systemID>/routes/<routeID>",methods = ['GET'])
+@auto.doc()
 def the_routes(systemID,routeID):
     '''shows a single route for the given system given *routeID*'''
     r = Routes.select().where(Routes.route_type==2).where(Routes.route_id==routeID)
@@ -78,45 +112,59 @@ def the_routes(systemID,routeID):
 
 #shows all the times for the times route
 @app.route("/api/<systemID>/routes/<routeID>/times",methods = ['GET'])
+@auto.doc()
 def the_route_times(systemID,routeID):
+    """shows all the times for the given route."""
     return jsonify({"message":"usage:TBD"})
  
  
 #shows cloest station to the person
 @app.route("/api/<systemID>/close/<lat>/<lon>",methods = ['GET'])
+@auto.doc()
 def the_closest_stop(systemID, lat, lon):
+    """Given Latitude and Logitude coords, returns the closest station"""
     s = TrainSearch.find_closest(lat, lon)
     return jsonify({"stop":s})
 
    
 #shows the given status for the routes
 @app.route("/api/<systemID>/routes/<routeID>/status",methods = ['GET'])
+@auto.doc()
 def the_route_status(systemID,routeID):
-    return jsonify({"message":"usage:TBD"})
-    
-#shows the given schedule for the routes for today
-@app.route("/api/<systemID>/routes/<routeID>/schedule",methods = ['GET'])
-def the_route_schedule(systemID,routeID):
+    """returns the details about a given route, including current status if available."""
     return jsonify({"message":"usage:TBD"})
     
 
-#shows the given status for the routes - third arg is day of week
+# @app.route("/api/<systemID>/routes/<routeID>/schedule",methods = ['GET'])
+# @auto.doc()
+# def the_route_schedule(systemID,routeID):
+#     """Shows the given schedule for the routes for today"""
+#     return jsonify({"message":"usage:TBD"})
+    
+
+#
 @app.route("/api/<systemID>/search_today/<from_station_id>/<to_station_id>",methods = ['GET'])
+@auto.doc()
 def the_route_search_today(systemID,from_station_id,to_station_id):
     s = TrainSearch.find_route(from_station_id, to_station_id, 0)
+    """shows the given status for the routes - third arg is day of week"""
     #request.data
     return jsonify({"trips":s})
     
-#shows the given status for the routes - third arg is day of week
-@app.route("/api/<systemID>/search/<from_station_id>/<to_station_id>/<dow>",methods = ['GET'])
+#
+@app.route("/api/<systemID>/search/<from_station_id>/<to_station_id>/<int:dow>",methods = ['GET'])
+@auto.doc()
 def the_route_search(systemID,from_station_id,to_station_id, dow):
+    """shows the given status for the routes for a gien day of the week. (0 = Monday)"""
     s = TrainSearch.find_route(from_station_id, to_station_id, dow)
     #request.data
     return jsonify({"trips":s})
 
-#shows the given status for the routes - third arg is day of week
+
 @app.route("/api/<systemID>/trips/<trip_id>/",methods = ['GET'])
+@auto.doc()
 def the_trip(systemID,trip_id):
+    """Shows details about a particular trip"""
     s = TrainSearch.find_trip(trip_id)
     #request.data
     return jsonify({"stops":s})
@@ -126,8 +174,11 @@ def the_trip(systemID,trip_id):
 
 #Lists all train stations
 @app.route("/api/<systemID>/stops",methods = ['GET'])
+@auto.doc()
 def all_stops(systemID):
+    """lists all the stops available in the network"""
     s = TrainSearch.get_stops() #request.data
+    
     all_r = []
     for rr in s:
         all_r.append(model_to_dict(rr))
@@ -135,7 +186,9 @@ def all_stops(systemID):
 
 #Lists all destination stations from origin station
 @app.route("/api/<systemID>/stops/<from_station>",methods = ['GET'])
+@auto.doc()
 def all_stops_from_origin(systemID, from_station):
+    """Returns all the stops from any given station"""
     s = TrainSearch.get_stops_from_origin(from_station) #request.data
     all_r = []
     for rr in s:
